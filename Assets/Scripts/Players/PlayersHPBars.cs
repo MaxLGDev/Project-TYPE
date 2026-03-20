e0709b65f27bf61afc9dabb814272c74b43888cf
Max <maxoumixou@gmail.com>
2026-03-20 23:59:32 +0900

    (add) added a ghost bar for HP bar for a smooth effect, and main bar changes color when less than 35% hp

--- Assets/Scripts/Players/PlayersHPBars.cs
+++ Assets/Scripts/Players/PlayersHPBars.cs
@@ -1,20 +1,73 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayersHPBars : MonoBehaviour
 {
     [SerializeField] private PlayerHealth playerHealth;
-    private Slider hpBar;
+    [SerializeField] private float smoothSpeed = 0.5f;
+
+    [SerializeField] private Image mainBarImage;
+    [SerializeField] private Image ghostBarImage;
+
+    private Color originalMainColor;
+    [SerializeField] private Color lowHPColor;
+
+    private Slider mainHPBar;
+    [SerializeField] private Slider ghostHPBar;
+
+    private Coroutine ghostBarCO;
 
     private void Start()
     {
-        hpBar = GetComponent<Slider>();
+        mainHPBar = GetComponent<Slider>();
 
-        hpBar.maxValue = playerHealth.MaxHP;
-        hpBar.value = playerHealth.CurrentHP;
+        originalMainColor = mainBarImage.color;
+        ResetBar();
+        
     }
 
-    private void RefreshHealthBar(float newHP) => hpBar.value = newHP;
+    private void RefreshHealthBar(float newHP)
+    {
+        mainHPBar.value = newHP;
+
+        if(newHP > playerHealth.MaxHP)
+        {
+            ResetBar();
+            return;
+        }
+
+        if (newHP > playerHealth.MaxHP * 0.35f)
+            mainBarImage.color = originalMainColor;
+        else
+            mainBarImage.color = lowHPColor;
+
+        if (ghostBarCO == null)
+            ghostBarCO = StartCoroutine(SmoothGhostHPBarCO());
+    }
+
+    public void ResetBar()
+    {
+        mainHPBar.maxValue = playerHealth.MaxHP;
+        mainHPBar.value = playerHealth.MaxHP;
+        mainBarImage.color = originalMainColor;
+
+        ghostHPBar.maxValue = playerHealth.MaxHP;
+        ghostHPBar.value = playerHealth.MaxHP;
+    }
+
+    private IEnumerator SmoothGhostHPBarCO()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        while (!Mathf.Approximately(ghostHPBar.value, mainHPBar.value))
+        {
+            ghostHPBar.value = Mathf.Lerp(ghostHPBar.value, mainHPBar.value, smoothSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        ghostHPBar.value = mainHPBar.value;
+    }
 
     private void OnEnable()
     {
